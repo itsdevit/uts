47823096e4b10a967d505bf917f230ce51f1d9b3
itsdevit <88619248+itsdevit@users.noreply.github.com>
2022-04-20 23:04:40 +0700

    Add files via upload

---
+++ uts.py
@@ -0,0 +1,13 @@
+print("tutorial looping")
+print("david apriansyah - 191011400559")
+
+print("menggunakan for")
+nums = [1, 5, 9, 11]
+for num in nums:
+    print(num)
+
+print("menggunankan while")
+num = 1
+while num < 10:
+    print(num)
+    num += 1  # berarti num = num + 1
